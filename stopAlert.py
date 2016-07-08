2cabd5f4b73dc2e6b82c466eef500d0b9a20c155
aoprisor <aoprisor@pentalog.fr>
2016-07-08 10:10:31 +0000

    Added stopAlert

---
+++ stopAlert.py
@@ -0,0 +1,10 @@
+import pexpect
+import time
+
+gatt = pexpect.spawn('gatttool -I')
+gatt.sendline('connect 84:DD:20:C5:7F:26')
+gatt.expect('Connection successful')
+
+gatt.sendline('char-write-cmd 0x0028 00:00')
+
+time.sleep(5)
